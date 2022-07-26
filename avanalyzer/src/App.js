6926137bf9acc5aab649d80141a52741822613e6
alandgmendes <alandgmendes@gmail.com>
2022-07-25 23:36:19 -0300

    chart draws open high low close

--- avanalyzer/src/App.js
+++ avanalyzer/src/App.js
@@ -8,32 +8,8 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">        
-        <Plot
-        data={[
-          {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
-            type: 'scatter',
-            mode: 'lines+markers',
-            marker: {color: 'red'},
-          },
-          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-        ]}
-        layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
-      />
-      <Plot
-        data={[{
-          values: [19, 26, 55],
-          labels: ['Residential', 'Non-Residential', 'Utility'],
-          type: 'pie'
-        }]}
-        layout = {{
-          height: 240,
-          width: 320
-        }}
-      />
-      <Intraday ticker={"arara"}/>
+      <header className="App-header">
+        <Intraday ticker={"NU"}/>
       </header>
     </div>
   );
--- avanalyzer/src/Components/IntraDay/IntraDay.js
+++ avanalyzer/src/Components/IntraDay/IntraDay.js
@@ -3,17 +3,25 @@ import Plot from 'react-plotly.js';
 
 function Intraday (props) {
     const [searched, setSearched] = useState(false);
-    const [dataFetched, setDataFetched] = useState({});
-    const [tickerItems, setTickerItems] = useState([]);
-    let tickerItens = [];
+    const [openTrace, setOpenTrace] = useState({});
+    const [highTrace, setHighTrace] = useState({});
+    const [closeTrace, setCloseTrace] = useState({});
+    const [lowTrace, setLowTrace] = useState({});
+    const [width, setWidth] = useState(window.innerWidth);
     let obj =[];
+    const widthView = Math.round(window.screen.width * 0.8);
     async function fetcher (ticker){
         var myHeaders = new Headers();
-        var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo';
+        var url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${ticker}&interval=60min&apikey=CB13MCAS13KOVKNU`;
         var myInit = { method: 'GET',
                headers: myHeaders,
                mode: 'cors',
                cache: 'default' };
+        let open =[];
+        let high =[];
+        let low = [];
+        let close = [];
+        let timeStamps = [];
         await fetch(url,myInit)
         .then(response => response.json())
         .then(data => {
@@ -24,43 +32,88 @@ function Intraday (props) {
             let innerArr = Object.entries(pointsArray[i]);
             let timeStamp = innerArr[0][1];
             let arrValues = Object.entries(innerArr[1][1]);
-            let lineItem = <li key={timeStamp}>
-                <span>{timeStamp}</span><span>{arrValues[0][0]}</span><span>{arrValues[0][1]}</span><span>{arrValues[1][0]}</span>
-                <span>{arrValues[1][1]}</span><span>{arrValues[2][0]}</span>
-                <span>{arrValues[2][1]}</span><span>{arrValues[3][0]}</span>
-                <span>{arrValues[3][1]}</span>
-            </li>;
-            tickerItens.push(lineItem);            
+            open.push(arrValues[0][1]);
+            high.push(arrValues[1][1]);
+            low.push(arrValues[2][1]);
+            close.push(arrValues[3][1]);
+            timeStamps.push(timeStamp);
+                      
           }
-          setTickerItems(tickerItens);
+          
+          setOpenTrace({
+            x: timeStamps,
+            y: open,
+            name: "open",
+            type: 'scatter',
+            mode: 'lines',
+            line: {
+              color: 'rgb(250, 1, 1)',
+              width: 3
+            }
+          });
+          setHighTrace({
+            x: timeStamps,
+            y: high,
+            name: "high",
+            type: 'scatter',
+            mode: 'lines',
+            line: {
+              color: 'rgb(1, 255, 1)',
+              width: 3
+            }
+          });
+          setLowTrace({
+            x: timeStamps,
+            y: low,
+            name: "low",
+            type: 'scatter',
+            mode: 'lines',
+            line: {
+              color: 'rgb(1, 1, 255)',
+              width: 3
+            }
+          });
+          setCloseTrace({
+            x: timeStamps,
+            y: close,
+            name: "close",
+            type: 'scatter',
+            mode: 'lines',
+            line: {
+              color: 'rgb(2, 1, 1)',
+              width: 3
+            }
+          });
           setSearched(true);
         })
         .catch(console.error);
     }
     useEffect(() => {
-        console.log(searched)
+      const handleWindowResize = () => setWidth(window.innerWidth)
+      window.addEventListener("resize", handleWindowResize);
+      return () => window.removeEventListener("resize", handleWindowResize);
+    }, []);
+
+    useEffect(() => {
+      
         if(!searched){
             fetcher(props.ticker);
         }
     })
 
 
+
     return(
-        <div>   
-            
-            {tickerItems}         
+        <div>
+            <h3>{props.ticker}</h3>
             <Plot
-                data={[
-                {
-                    x: [1, 2, 3],
-                    y: [2, 6, 3],
-                    type: 'scatter',
-                    mode: 'lines+markers',
-                    marker: {color: 'red'},
-                },
-                {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-                ]}
-                layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+              data={[
+                openTrace,
+                lowTrace,
+                highTrace,
+                closeTrace
+              ]}
+              layout={ {width: width, height: 440, title: props.ticker} }
             />
         </div>
     );
